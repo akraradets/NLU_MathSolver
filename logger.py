41577a65fdd9b8c599ab40a6d5625020d383ea3f
akraradets <akraradets@gmail.com>
2019-07-05 17:05:33 +0700

    set logger

--- logger.py
+++ logger.py
@@ -5,21 +5,21 @@ import logging
 # logging.error('This is an error message')
 # logging.critical('This is a critical message')
 
+
 class Logger:
     def __init__(self, className):
-        logging.basicConfig(format='%(asctime)s - %(message)s',
+        logging.basicConfig(format='%(asctime)s - %(message)s', filename='log.txt',
                             datefmt='%Y%m%d %H:%M:%S', level=logging.DEBUG)
+        logging.getLogger().addHandler(logging.StreamHandler())
         self.className = className
 
     def log(self, level, message):
 
         if level not in ["debug", "info", "warning", "error", "critical"]:
-            print('asd')
             raise Exception(
                 "Logger level on accept [debug,info,warning,error,critical].")
         result = getattr(logging, level)(
-            "class="+self.className + "|message=" + message)
-
+            f'class={self.className}|message={message}')
 
 # logger = Logger("Logger")
 # logger.log("debug", 'Test Message')
