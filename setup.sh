774dbabf62335b174c6180bd2e1795abe36423b4
akraradets <akraradets@gmail.com>
2019-07-12 21:20:37 +0700

    write setup to download stanford libs

---
+++ setup.sh
@@ -0,0 +1,42 @@
+FILE="stanford-parser-full-2018-10-17.zip"
+DIR="stanford-parser-full-2018-10-17"
+LINK="stanford-parser"
+
+if [ -d "./libs" ] 
+then
+    echo "Directory ./libs exists." 
+else
+    mkdir libs
+    echo "Create ./libs" 
+fi
+
+cd libs
+
+if [ -f "$FILE" ]
+then
+    echo "File $FILE exists." 
+else
+    echo "Download $FILE" 
+    wget http://nlp.stanford.edu/software/$FILE
+fi
+
+if [ -d "$DIR" ] 
+then
+    echo "Directory $DIR exists." 
+else
+    echo "unzip $FILE"
+    unzip $FILE
+fi
+
+if [ -L "$LINK" ]
+then
+    rm $LINK
+fi
+
+echo "Create symlink"
+ln -s $DIR $LINK
+
+echo "Remove $FILE"
+rm $FILE
+
+echo "SUCCESS....!!!"
