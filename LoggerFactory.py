568273232435e1dfe71ca3c49a8bd98151cef9f9
akraradets <akraradets@gmail.com>
2019-11-29 13:21:57 +0700

    update log system

--- LoggerFactory.py
+++ LoggerFactory.py
@@ -1,17 +1,18 @@
 import logging
+import datetime
 # logging.debug('This is a debug message')
 # logging.info('This is an info message')
 # logging.warning('This is a warning message')
 # logging.error('This is an error message')
 # logging.critical('This is a critical message')
 
-
 class LoggerFactory:
     def __init__(self, callerClass):
         format = '%(asctime)s-%(name)s(%(funcName)s)-%(levelname)s|%(message)s'
-        file = 'log.txt'
+        path = './logs/'
+        file = datetime.datetime.now().strftime('%Y%m%d') + '.log'
         dateFormat = '%Y%m%d %H:%M:%S'
-        logging.basicConfig(format=format, filename=file, datefmt=dateFormat)
+        logging.basicConfig(format=format, filename=path+file, datefmt=dateFormat)
         self.logger = logging.getLogger(callerClass.__class__.__name__)
         self.logger.setLevel(logging.DEBUG)
         
