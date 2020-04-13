b46343ab047d258bebd31041769aa772ad2180b0
akraradets <akraradets@gmail.com>
2020-04-13 17:19:50 +0700

    Done SRL

--- SRL.py
+++ SRL.py
@@ -19,20 +19,91 @@ class SRL:
         self.predictor = Predictor.from_path("libs/allennlp-SRL.tar.gz")
         SRL.__instance = self
 
-  def tag(self, sentence):
+  def tag(self, sentence, withAux = False):
+    # reset results
+    self.results = None
     results = self.predictor.predict(
       sentence=sentence
     )
+    # save results
+    self.results = results
 
-    for verb in results['verbs']:
-      if len(verb['tags']) == verb['tags'].count('O') + 1 :
-          continue
-      print(f"======{verb['verb']}=======")
-      for word, tag in zip(results["words"], verb["tags"]):
-          print(f"{word}\t{tag}")
+    words = results['words']
+    verbs = []
+    tags = {}
+    roles = {}
+
+    for target in results['verbs']:
+      tagged_words = []
+      roles_set = set()
+      isAux = False
+      if len(target['tags']) == target['tags'].count('O') + 1:
+        isAux = True
+      
+      if isAux and withAux == False:
+        continue
+
+      for word, tag in zip(words, target['tags']):
+        suffix = ''
+        role = tag
+        if tag.find('-') >= 0:
+          suffix, role = tag.split('-',1)
+
+        tag = {'word':word,
+          'tag':tag,
+          'suffix': suffix,
+          'role': role}
+        tagged_words.append(tag)
+        roles_set.add(role)
+
+      verbs.append(target['verb'])
+      tags[target['verb']] = tagged_words
+      roles[target['verb']] = roles_set
+
+    self.words = words
+    self.verbs = verbs
+    self.tags = tags
+    self.roles = roles
+
+    return self.verbs
+  
+  def getRoleSet(self, verb):
+    role_set = []
+    try:
+      role_set = self.roles[verb]
+    except KeyError:
+      raise KeyError(f"{verb} is not found in {self.verbs}. \n The sentence is {self.words}")
+
+    return role_set
+
+  def getRole(self, role, verb, suffix = None):
+    # return an array of word that tagged as the query role and verb
+    tagged_words = []
+
+    try:
+      tagged_words = self.tags[verb]
+    except KeyError:
+      raise KeyError(f"{verb} is not found in {self.verbs}. \n The sentence is {self.words}")
+
+    # print(tagged_words)
+
+    output = []
+    for tagged_word in tagged_words:
+      # print(tagged_word)
+      if(tagged_word['role'] == role):
+        output.append(tagged_word['word'])
+
+    return output
+    # return 'e'
 
 # srl = SRL.getInstance()
-# srl.tag("Sam eats 3 apples.")
-
-# srl2 = SRL.getInstance()
-# srl2.tag("Sam eats 3 apples.")
+# verbs = srl.tag("Did Uriah honestly think he could beat The Legend of Zelda in under three hours?")
+# print(verbs)
+# print("== Role ==")
+# print(srl.getRoleSet('think'))
+# print("==== think ====")
+# print(srl.getRole('ARG0','think'))
+# print(srl.getRole('ARG1','think'))
+# print("==== beat ====")
+# print(srl.getRole('ARG0','beat'))
+# print(srl.getRole('ARG1','beat'))
